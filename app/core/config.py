f7ad59359b91f60b357b9221d144c37221864eae
Erdem Demirkapı <demirkapierdem@gmail.com>
2024-08-11 16:45:31 +0300

    Implement logging

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,17 @@
 import logging
+import os
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     APP_NAME: str = "FastAPI Image Transformation"
-    API_V1_STR: str = "/api/v1"
+    API_V1: str = "/api/v1"
 
     LOGGING_LEVEL: str = "INFO"
+    LOG_FILE: str = os.getenv("LOG_FILE", default="app.log")
+    LOG_TO_FILE: bool = os.getenv("LOG_TO_FILE", default=True)
+    LOG_TO_CONSOLE: bool = os.getenv("LOG_TO_CONSOLE", default=True)
 
     class Config:
         env_file = ".env"
@@ -14,7 +19,15 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+log_handlers = []
+if settings.LOG_TO_CONSOLE:
+    log_handlers.append(logging.StreamHandler())
+
+if settings.LOG_TO_FILE:
+    log_handlers.append(logging.FileHandler(settings.LOG_FILE))
+
 logging.basicConfig(
     level=settings.LOGGING_LEVEL,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=log_handlers
 )
--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.core.config import settings
 
 app = FastAPI(title=settings.APP_NAME)
 
-app.include_router(image_transformation_router, prefix=settings.API_V1_STR)
+app.include_router(image_transformation_router, prefix=settings.API_V1)
 
 logger = logging.getLogger(__name__)
 
